da63a4b53efca2f8035ad8c37321d5a74f6c0c7f
Jakub Pachut <jpachut@gmail.com>
2025-11-04 21:06:08 +0100

    adding more routes and models and services

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,19 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/',)]
-    public function index(): Response
+    #[Route('/')]
+    public function index(StarshipRepository $starshipRepository): Response
     {
-        $starshipCount = 420;
+        $starships = $starshipRepository->findAll();
+
+        $myStarship = $starships[array_rand($starships)];
 
         return $this->render('main/index.html.twig', [
-            'starshipCount' => $starshipCount,
+            'myStarship' => $myStarship,
+            'starships' => $starships
         ]);
     }
 }
---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Starship;
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/api/starships')]
+class StarshipApiController extends AbstractController
+{
+    #[Route('', methods: ['GET'])]
+    public function getCollection(StarshipRepository $repository): Response
+    {
+        
+        $starships = $repository->findAll();
+
+        return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    public function get(int $id) {
+        dd($id);
+    }
+}
